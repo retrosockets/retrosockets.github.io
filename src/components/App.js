8afb4aa814e4928629223c65f0d45ae4392514d1
Alan Smith <a.bax.smith@gmail.com>
2016-12-23 12:05:54 -0700

    add react

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../assets/stylesheets/base.scss';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>RetroSockets</h1>
+        <p>You've got feels. We've got websockets.</p>
+      </div>
+    )
+  }
+};
+
+export default App;
